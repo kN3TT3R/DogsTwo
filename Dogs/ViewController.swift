ec575a7e2daebbbde16358eac64898fef82b81c7
VDAB Cursist <vdabcursist@vdabs-imac-6.local>
2017-02-02 10:57:02 +0100

    Initial commit

--- Dogs/ViewController.swift
+++ Dogs/ViewController.swift
@@ -11,13 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    //MARK: - Global properties
-    let dogNames = ["Chuck", "Wobbes", "Wimpie", "Happy"]
-    let dogImageArray = [#imageLiteral(resourceName: "Chuck"), #imageLiteral(resourceName: "Happy"), #imageLiteral(resourceName: "Wobbes"), #imageLiteral(resourceName: "Wimpie")]
-    let dogDescriptionText = " , lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+    // MARK: - Global properties
+    let dogNameArray = ["Chuck", "Wobbes", "Wimpie", "Happy"]
+    //let dogImageArray = [#imageLiteral(resourceName: "Chuck"), #imageLiteral(resourceName: "Happy"), #imageLiteral(resourceName: "Wobbes"), #imageLiteral(resourceName: "Wimpie")]
+    
+    
+    // Version 1.2 Beta
+    let dogImageArray = [("Chuck", #imageLiteral(resourceName: "Chuck")),
+                         ("Wobbes", #imageLiteral(resourceName: "Happy")),
+                         ("Wimpie",#imageLiteral(resourceName: "Wobbes")),
+                         ("Happy", #imageLiteral(resourceName: "Wimpie"))]
+    
+    let dogDescriptionText = " , lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    /*
+     "Ik ben dog.name. Ik kom uit dog.location. Net zoals mijn moeder ben ik een dog.race. Ik ben nu dog.age jaar oud."
+     */
+    
+    
+    
     var dog = Dog()
     var dogCounter = 0
     
+    
+    // MARK: - Structures
     /// Structure Dog
     /// properties
     ///     - name: the name of the dog
@@ -31,9 +47,9 @@ class ViewController: UIViewController {
     
     
     // MARK: - IBOutlets
-    @IBOutlet weak var dogName: UILabel!
-    @IBOutlet weak var dogImage: UIImageView!
-    @IBOutlet weak var dogDescription: UITextView!
+    @IBOutlet weak var dogNameLabel: UILabel!
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var dogDescriptionView: UITextView!
     
     
     // MARK: - IBAction
@@ -47,7 +63,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loadNextDog()
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -64,15 +79,21 @@ class ViewController: UIViewController {
         if dogCounter == dogImageArray.count {
             dogCounter = 0
         }
-        dog.name = dogNames[dogCounter]
+        dog.name = dogNameArray[dogCounter]
         dog.description = dog.name + dogDescriptionText
         dog.image = dogImageArray[dogCounter]
         
-        dogName.text = dog.name
-        dogImage.image = dog.image
-        dogDescription.text = dog.description
+        updateView()
         
         dogCounter += 1
+    }
+    
+    
+    /// Function which will update the view based on a specific instance of Dog
+    func updateView() {
+        dogNameLabel.text = dog.name
+        dogImageView.image = dog.image
+        dogDescriptionView.text = dog.description
     }
 }
 
