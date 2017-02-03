4ed26f2cea5a740c631828b2f1c7b245dfbe5c0a
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2017-02-03 22:52:41 +0100

    Changes Navigation

--- AnimalViewController.swift
+++ AnimalViewController.swift
@@ -15,51 +15,35 @@ class AnimalViewController: UIViewController {
     @IBOutlet weak var animalNameLabel: UILabel!
     @IBOutlet weak var animalAgeLabel: UILabel!
     @IBOutlet weak var animalBreedLabel: UILabel!
-    
     @IBOutlet weak var animalImageView: UIImageView!
     
     
-    // MARK: - Global Variables
-    /* 
-        We need to create the initial data for the first animal to be displayed.
-        -> create a variable for animalInformation (Tuple)
-                because in ViewController we use a Dictionary of Tuples containing animals:
-                let dogInfoDictionary = ["Chuck": (birthPlace: "Torhout", breed: "Berner-Sennen", age: 6), ...]
-        -> create a variable for animalName (String)
-                because the Tuple does not yet contain the animals name
-        -> now go to the viewDidLoad()
-     */
-    var animalInformation: (birthPlace: String, breed: String, age: Int) =
-        (birthPlace: "Torhout", breed: "Berner-Sennen", age: 6)
-    var animalName = "Chuck"
-    let dogImageArray = [#imageLiteral(resourceName: "Chuck"), #imageLiteral(resourceName: "Happy"), #imageLiteral(resourceName: "Wobbes"), #imageLiteral(resourceName: "Wimpie")]
+    // MARK: - Structure
+    struct Dog {
+        var name = ""
+        var birthPlace = ""
+        var breed = ""
+        var age = 0
+        var description = ""
+        var image = UIImage()
+    }
+
+    
+    // MARK: - Global Properties
+    var dog = Dog()
     
     
     //  MARK: - Overridden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-            fill the 3 labels and the imageview with the initial data
-        */
-        self.animalNameLabel.text = animalName
-        self.animalAgeLabel.text = "\(animalInformation.age) years"
-        self.animalBreedLabel.text = animalInformation.breed
-        self.animalImageView.image = dogImageArray[0]
+        self.animalNameLabel.text = dog.name
+        self.animalAgeLabel.text = "\(dog.age) years"
+        self.animalBreedLabel.text = dog.breed
+        self.animalImageView.image = dog.image
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
--- Dogs/ViewController.swift
+++ Dogs/ViewController.swift
@@ -1,8 +1,6 @@
 //
 //  ViewController.swift
 //  Dogs
-//  Version 1.2 - Tuples
-//  Version 1.3 - Dictionary
 //
 //  Created by Kenneth Debruyn on 10/01/17.
 //  Copyright © 2017 kN3TT3R. All rights reserved.
@@ -13,40 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    // MARK: - Global properties
-    let dogImageArray = [#imageLiteral(resourceName: "Chuck"), #imageLiteral(resourceName: "Happy"), #imageLiteral(resourceName: "Wobbes"), #imageLiteral(resourceName: "Wimpie")]
-    
-    // Version 1.2
-    //    let dogInfoArray = [ (name: "Chuck", birthPlace: "Torhout", breed: "Berner-Sennen", age: 6),
-    //                         (name: "Wobbes", birthPlace: "Werchter", breed: "Jack-Russell", age: 4),
-    //                         (name: "Wimpie", birthPlace: "Leuven", breed: "Labrador", age: 2),
-    //                         (name: "Happy", birthPlace: "Brugge", breed: "Golden Retriever", age: 8)]
-    
-    // Version 1.3
-    /// dogInfoDictionary: a Dictionary
-    ///     - key is the name of the dog
-    ///     - value is a Tuple containing (birthplace, breed, age)
-    let dogInfoDictionary = [
-        "Chuck": (birthPlace: "Torhout", breed: "Berner-Sennen", age: 6),
-        "Wobbes": (birthPlace: "Werchter", breed: "Jack-Russell", age: 4),
-        "Wimpie": (birthPlace: "Leuven", breed: "Labrador", age: 2),
-        "Happy": (birthPlace: "Brugge", breed: "Golden Retriever", age: 8)
-    ]
-    let dogNames = ["Chuck", "Wobbes", "Wimpie", "Happy"]
-    
-    var dog = Dog()
-    var dogCounter = 0
+    // MARK: - IBOutlets
+    @IBOutlet weak var dogNameLabel: UILabel!
+    @IBOutlet weak var dogImageView: UIImageView!
     
     
-    // MARK: - Structures
-    /// Structure Dog
-    /// properties
-    ///     - name: the name of the dog
-    ///     - birthPlace: the birthplace of the dog
-    ///     - breed: the breed of the dog
-    ///     - description: the description of the dog
-    ///     - age: the age of the dog
-    ///     - image: the image for the dog
+    // MARK: - Structure
     struct Dog {
         var name = ""
         var birthPlace = ""
@@ -57,9 +27,17 @@ class ViewController: UIViewController {
     }
     
     
-    // MARK: - IBOutlets
-    @IBOutlet weak var dogNameLabel: UILabel!
-    @IBOutlet weak var dogImageView: UIImageView!
+    // MARK: - Global Properties
+    let dogInfoDictionary = [
+        "Chuck": (birthPlace: "Torhout", breed: "Berner-Sennen", age: 6),
+        "Wobbes": (birthPlace: "Werchter", breed: "Jack-Russell", age: 4),
+        "Wimpie": (birthPlace: "Leuven", breed: "Labrador", age: 2),
+        "Happy": (birthPlace: "Brugge", breed: "Golden Retriever", age: 8)
+    ]
+    let dogNames = ["Chuck", "Wobbes", "Wimpie", "Happy"]
+    let dogImageArray = [#imageLiteral(resourceName: "Chuck"), #imageLiteral(resourceName: "Happy"), #imageLiteral(resourceName: "Wobbes"), #imageLiteral(resourceName: "Wimpie")]
+    var dog = Dog()
+    var dogCounter = 0
     
     
     // MARK: - IBAction
@@ -77,43 +55,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loadNextDog()
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        if segue.identifier == "showDogDetail" {
+            let destinationVC = segue.destination as! AnimalViewController
+            destinationVC.dog.name = self.dog.name
+            destinationVC.dog.age = self.dog.age
+            destinationVC.dog.breed = self.dog.breed
+            destinationVC.dog.image = self.dog.image
+        }
     }
     
     
     // MARK: - Homemade Functions
-    /// Function which will:
-    ///     -> create a new instance of Dog
-    ///     -> add a name, description and image to the dog based on the Tuple
-    ///     -> call updateView to update the info to the view
     func loadNextDog() {
-        
-        // checks on 'index out of bound'
         if dogCounter == dogImageArray.count {
             dogCounter = 0
         }
-        
-        // Version 1.2
-        //        let dogInfoTuple = dogInfoDictionary[dogCounter]
-        //        dog.name = dogInfoTuple.name
-        //        dog.birthPlace = dogInfoTuple.birthPlace
-        //        dog.breed = dogInfoTuple.breed
-        //        dog.age = dogInfoTuple.age
-        //        dog.description = "Ik ben \(dog.name). Ik kom uit \(dog.birthPlace). Net zoals mijn moeder ben ik een \(dog.breed). Ik ben nu \   (dog.age) jaar oud."
-        
-        
-        // Version 1.3
+
         let dogName = dogNames[dogCounter]
         let dogInfoTuple = dogInfoDictionary[dogName]
         dog.name = dogName
         dog.birthPlace = dogInfoTuple!.birthPlace
         dog.breed = dogInfoTuple!.breed
         dog.age = dogInfoTuple!.age
-        dog.description = "Ik ben \(dog.name). Ik kom uit \(dog.birthPlace). Net zoals mijn moeder ben ik een \(dog.breed). Ik ben nu \(dog.age) jaar oud."
+        dog.description = "Ik ben \(dog.name). Ik kom uit \(dog.birthPlace). " +
+            "Net zoals mijn moeder ben ik een \(dog.breed). Ik ben nu \(dog.age) jaar oud."
         dog.image = dogImageArray[dogCounter]
         
         updateView()
@@ -121,26 +94,13 @@ class ViewController: UIViewController {
         dogCounter += 1
     }
     
-    /// Function which will:
-    ///     -> create a new instance of Dog
-    ///     -> add a name, description and image to the dog based on the Tuple
-    ///     -> call updateView to update the info to the view
     func loadPreviousDog() {
-        // checks on 'index out of bound'
         if dogCounter == 0 {
             dogCounter = dogImageArray.count
         }
         
         dogCounter -= 1
-        
-        // Version 1.2
-        //        let dogInfoTuple = dogInfoDictionary[dogCounter]
-        //        dog.name = dogInfoTuple.name
-        //        dog.birthPlace = dogInfoTuple.birthPlace
-        //        dog.breed = dogInfoTuple.breed
-        //        dog.age = dogInfoTuple.age
-        
-        // Version 1.3
+
         let dogName = dogNames[dogCounter]
         dog.name = dogName
         dog.image = dogImageArray[dogCounter]
@@ -148,7 +108,6 @@ class ViewController: UIViewController {
         updateView()
     }
     
-    /// Function which will update the view based on a specific instance of Dog
     func updateView() {
         dogNameLabel.text = dog.name
         dogImageView.image = dog.image
